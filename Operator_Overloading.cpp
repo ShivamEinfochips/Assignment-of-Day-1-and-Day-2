2498bf9bd2efdca8b81a44f2b653520243111dbf
Shivam Pandey <99633749+ShivamEinfochips@users.noreply.github.com>
2022-02-22 12:19:52 +0530

    Add files via upload

---
+++ Operator_Overloading.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+ 
+class ABC
+{
+    int x,y;
+    public:
+        ABC()
+        {
+            x=0;
+            y=0;
+        }
+        //Parameterized Constructor
+        ABC(int a,int b)
+        {
+            x=a;
+            y=b;
+        }
+        void disp()
+        {
+            cout<<"X = "<<x<<" Y = "<<y<<endl;
+        }
+        
+        //overloading '+' operator
+        ABC operator + (ABC a)
+        {
+            ABC temp;
+            temp.x = x + a.x;
+            temp.y = y + a.y;
+            return temp;
+        }//end of overloading fuction
+        ABC operator / (ABC a)
+        {
+            ABC temp;
+            temp.x = x / a.x;
+            temp.y = y / a.y;
+            return temp;
+        }//end of overloading fuction
+        ABC operator - (ABC a)
+        {
+            ABC temp;
+            temp.x = x - a.x;
+            temp.y = y - a.y;
+            return temp;
+        }//end of overloading fuction
+        ABC operator * (ABC a)
+        {
+            ABC temp;
+            temp.x = x * a.x;
+            temp.y = y * a.y;
+            return temp;
+        }//end of overloading fuction
+}; // end of class ABC
+int main()
+{
+    ABC a1(2,3),a2(10,5);
+    ABC a3;
+    a1.disp();
+    a2.disp();
+    cout<<"Addition"<<endl;
+    a3 = a1+a2;
+    a3.disp();
+    cout<<"Multiplication"<<endl;
+    a3=a1*a2;
+    a3.disp();
+    cout<<"Subraction"<<endl;
+    a3=a2-a1;
+    a3.disp();
+    return 0;
+}
